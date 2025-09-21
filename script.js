1d5dae0de5a1b23c0f4ded28a79d3c7d44f32a52
Belvoa <Yanis.grf@hotmail.com>
2025-09-21 13:14:02 +0200

    Initial Commit

---
+++ script.js
@@ -0,0 +1,84 @@
+const displayBox = document.querySelector(".display"),
+    displayInput = document.querySelector(".display-input"),
+    displayResult = document.querySelector(".display-result"),
+    buttons = document.querySelectorAll("button"),
+    operators = ["%", "÷", "×", "-", "+"];
+
+let input = "",
+    result = "",
+    lastCalculation = false;
+
+//logique des calculs
+const calculate = btnValue => {
+    const lastChar = input.slice(-1),
+        secondToLastChar = input.slice(-2, -1),
+        withoutLastChar = input.slice(0, -1),
+        isLastCharOperator = operators.includes(lastChar),
+        isInvalidResult = ["Error", "Infinity"].includes(result);
+
+    if (btnValue === "=") {
+        if(input === "" || lastChar === "." || lastChar === "(" || isLastCharOperator && lastChar !== "%" || lastCalculation)
+            return;
+
+        const formattedInput = replaceOperators(input);
+        try {
+            const calculatedValue = eval(formattedInput);
+            result = parseFloat(calculatedValue.toFixed(10)).toString();
+        }
+        catch {
+            result = "Error";
+        }
+
+        input += btnValue;
+        lastCalculation = true;
+        displayBox.classList.add("active");
+    }
+
+    else if (btnValue === "AC") {
+        resetCalculator("");
+    }
+
+    else if (btnValue ==="") {
+        if(lastCalculation){
+            if (isInvalidResult)
+                resetCalculator("");
+            resetCalculator(result.slice(0,-1));
+        }
+        else input = withoutLastChar;
+    }
+    else if (operators.includes(btnValue)){
+        if(lastCalculation){
+            resetCalculator(result + btnValue) ;
+        }
+        else{
+            input += btnValue;
+        }
+    }
+    else{
+        if (lastCalculation)
+            resetCalculator(btnValue);
+        else
+            input += btnValue;
+    }
+
+
+    //update display
+    displayInput.value = input;
+    displayResult.value = result;
+    displayInput.scrollLeft = displayInput.scrollWidth;
+};
+
+//Remplacer / et *
+const replaceOperators = input => input.replaceAll("÷", "/").replaceAll("×","*");
+
+const resetCalculator = newInput => {
+    input = newInput;
+    result = "";
+    lastCalculation = false;
+    displayBox.classList.remove("active");
+}
+
+//Rendre utile chaque bouton
+buttons.forEach(button =>
+    button.addEventListener("click", e => calculate(e.target.textContent))
+);
